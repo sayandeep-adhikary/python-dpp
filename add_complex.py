4319c11bb1f758f36c6d5a7c6a1491a24aeeb0b2
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-01-09 12:41:25 +0530

    Add files via upload

---
+++ add_complex.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the REAL part of the 1st number : "))
+b = (input("Enter the IMAGINARY part of the 1st number: "))
+x = int(input("Enter the REAL part of the 2nd number : "))
+y = (input("Enter the IMAGINARY part of the 2nd number: "))
+real = a + x
+imaginary = int(b[0]) + int(y[0])
+print(f"({a} + {b}i) + ({x} + {y}i) = {real} + {imaginary}i")
---
+++ rectangle.py
@@ -0,0 +1,4 @@
+a = int(input("Enter the length : "))
+b = int(input("Enter the breadth : "))
+print("The area of Rectangle is ", a*b)
+print("The perimeter of Rectangle is ", 2*(a+b))
