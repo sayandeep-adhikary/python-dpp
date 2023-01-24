775ee2f90827c0c75e884629c53a60a1b535bca0
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-01-24 16:45:56 +0530

    Add files via upload

---
+++ sort3nos.py
@@ -0,0 +1,24 @@
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+c = int(input("Enter 3rd number : "))
+print("The sorted list is : ")
+if a > b and a > c:
+    max = a
+else:
+    if b > a and b > c:
+        max = b
+    else:
+        max = c
+        
+        
+if a < b and a < c:
+    min = a
+else:
+    if b < a and b < c:
+        min = b
+    else:
+        min = c
+        
+        
+mid = (a+b+c) - max - min
+print(min, mid, max)
