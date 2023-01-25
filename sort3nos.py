7134de07eebd56cad22b5a6f716553f27c8df282
Sayandeep Adhikary <sayandeepadhikary2003@gmail.com>
2023-01-25 16:42:42 +0530

    .

--- sort3nos.py
+++ sort3nos.py
@@ -1,7 +1,7 @@
 a = int(input("Enter 1st number : "))
 b = int(input("Enter 2nd number : "))
 c = int(input("Enter 3rd number : "))
-print("The Sorted list is : ")
+print("The Sorted List is : ")
 if a > b and a > c:
     max = a
 else:
